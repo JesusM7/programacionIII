3cb0de5fe22e9e128b68e26c2320f02c7063199a
Jesus Medina <medinajesusg821@gmail.com>
2022-06-14 07:55:43 -0400

    database

--- database/db.js
+++ database/db.js
@@ -1,11 +1,6 @@
-const mysql = require ("mysql");
+const mysql = require ("mysql2");
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect((error) => {
     if(error){
